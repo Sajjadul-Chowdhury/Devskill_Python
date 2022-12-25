21797483eb5aad23d9100854630c99f35824cdb0
Sajjadul-Chowdhury <95188348+Sajjadul-Chowdhury@users.noreply.github.com>
2022-12-25 15:05:02 +0100

    Update DevSkill_MiniProject_1.py
    version: 1

--- DevSkill_MiniProject_1.py
+++ DevSkill_MiniProject_1.py
@@ -1,3 +1,44 @@
+class accountHolder():
+    def __init__(self, cardNumber, pin, firstname, lastname, balance):
+        self.cardNumber = cardNumber
+        self.pin = pin
+        self.firstname = firstname
+        self.lastname = lastname
+        self.balance = balance
+    
+   
+    # Method for getting user information
+   
+    def get_cardNumber(self):
+        return self.cardNumber
+    def get_pin(self):
+        return self.pin
+    def get_firstname(self):
+        return self.firstname
+    def get_lastname(self):
+        return self.lastname
+    def get_balance(self):
+        return self.balance
+
+    # setting user information
+    def set_cardNumber(self, newValue):
+        self.cardNumber = newValue
+    def set_pin(self, newValue):
+        self.pin = newValue
+    def set_firstnamer(self, newValue):
+        self.firstname = newValue
+    def set_lastname(self, newValue):
+        self.lastname = newValue
+    def set_balance(self, newValue):
+        self.balance = newValue
+
+
+    def display(self):
+        print( "card Number: ", self.cardNumber)
+        print( "Pin: ", self.pin)
+        print( "First Name: ", self.firstname)
+        print( "Last Name: ", self.lastname)
+        print( "Balance: ", self.balance)
 
 
 # this function will ask the uuser for the reason for using ATM card 
@@ -12,13 +53,14 @@ def Home_Screen():
 # this function will ask the user regarding the amount for deposit
 '''
     -----------------------------------------------------------------------------
-    Pending Task: Adding the current amount with the previous amount and validate
+     Task: Adding the current amount with the previous amount and validate
     -----------------------------------------------------------------------------
 '''
-def deposit():
+def deposit(accountHolder):
     try:
         deposit= float(input('Enter the ammount that you would like to deposit: '))
-
+        accountHolder.set_balance(accountHolder.get_balance() + deposit)
+        print('Your new balance is: ', str(accountHolder.get_balance()))
     except:
         print('Invalid User Input ')
    
@@ -26,13 +68,18 @@ def deposit():
 # this function will ask the user regarding the amount for withdraw
 '''
     --------------------------------------------------------------------------------------
-    Pending Task: 1. checking the current available amount, 
-                  2. validate regarding the user is allowed to withdraw the desired amount
+     Task: 1. checking the current available amount, 
+           2. validate regarding the user is allowed to withdraw the desired amount
     --------------------------------------------------------------------------------------
 '''
-def withdraw():
+def withdraw(accountHolder):
     try:
         withdraw= float(input('Enter the ammount that you would like to withdraw: '))
+        if(accountHolder.get_balance()< withdraw):
+            print('Insufficient balance: ')
+        else:
+            accountHolder.set_balance(accountHolder.get_balance - withdraw)
+            print('Thank You')
 
     except:
         print('Invalid User Input ')
@@ -41,13 +88,70 @@ def withdraw():
 # this function will show the current balance
 '''
     --------------------------------------------------------------------------------------
-    Pending Task: 1. function which is holds all users current balance information 
+     Task: 1. function which is holds all users current balance information 
                   
     --------------------------------------------------------------------------------------
 '''
-def available_balance():
-    print('Your current balance is: ')
+def available_balance(accountHolder):
+    print('Your current balance is: ', accountHolder.get_balance())
 
-# testing the functionality of the function
-Home_Screen()
-deposit()
+if __name__ == "__main__":
+    present_user = accountHolder("", "", "", "", "")
+
+    # User Database
+    accountHolder_list = []
+    accountHolder_list.append(accountHolder("100200", 1470, "Lily", "Homes", 15.58))
+    accountHolder_list.append(accountHolder("300400", 2583, "Joe", "Marshal", 95.20))
+    accountHolder_list.append(accountHolder("500600", 3691, "John", "Smith", 500.0))
+
+# testing the functionality 
+
+    # 1. Debit card number validation
+    debitCardNumber = " "
+    while True:
+        try:
+            debitCardNumber = input (" Please enter your debit card number: ")
+            debitMatch = [ dnumber for dnumber in accountHolder_list if dnumber.cardNumber == debitCardNumber ]
+            # for dnumber in accountHolder_list:
+            #     if dnumber.cardNumber== debitCardNumber:
+            #         debitMatch.append(dnumber) 
+            if (len(debitMatch)>0):
+                present_user = debitMatch[0]
+                break
+            else:
+                print(" Card number not valid. Please Try again")
+
+        except:
+                print(" Card number not valid. Please Try again")
+
+    #2. Pin validation
+    while True:
+        try: 
+            uPin = int(input("Please enter your pin: ").strip())
+            if(present_user.get_pin()== uPin):
+                break
+            else:
+                 print(" Enter a valid pin")
+        except:
+            print(" Enter a valid pin")
+
+
+    print( "Welcome to ATM ", present_user.get_firstname())
+    options = 0
+    while (options != 4):
+        Home_Screen()
+        try:
+            options = int ( input()) 
+        except:
+            print("Invalid input from the user. Please try again.")
+        if (options ==1):
+            deposit(present_user)
+        elif (options == 2):
+            withdraw(present_user)
+        elif (options == 3):
+            available_balance(present_user)
+        elif (options == 4):
+            break
+        else:
+            options = 0
+print( "Thanks for using our ATM. ")
